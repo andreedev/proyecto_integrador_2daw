918a16e13fb35d283168e8158c1eda499e4dc658
cris <54333719+andreedev@users.noreply.github.com>
2026-01-12 23:35:05 +0100

    fix en cerrar modales

--- js/admin-eventos.js
+++ js/admin-eventos.js
@@ -1,5 +1,7 @@
 const modalVerDetalleEvento = document.querySelector('#modalVerDetalleEvento');
 
+const closeModalList = modalVerDetalleEvento.querySelectorAll('.close-modal');
+
 closeModalList.forEach(closeModal => {
     closeModal.addEventListener('click', () => {
         const dialog = closeModal.closest('dialog');
