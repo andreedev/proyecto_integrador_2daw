f704340e61dd301904c658cfdc250d2bd2258538
maria <mariaqingxuan@gmail.com>
2026-01-12 23:57:58 +0100

    cambios en admin-eventos

--- js/admin-eventos.js
+++ js/admin-eventos.js
@@ -2,6 +2,14 @@ const modalVerDetalleEvento = document.querySelector('#modalVerDetalleEvento');
 
 const closeModalList = modalVerDetalleEvento.querySelectorAll('.close-modal');
 
+const eventoContainer = document.querySelector('.event-row');
+const btnEliminarEvento = document.querySelector('.neutral-button');
+const btnEditarEvento = document.querySelector('.primary-button');
+
+const btnCrearEvento = document.getElementById('btnCrearEvento');
+
+
+
 closeModalList.forEach(closeModal => {
     closeModal.addEventListener('click', () => {
         const dialog = closeModal.closest('dialog');
@@ -9,3 +17,15 @@ closeModalList.forEach(closeModal => {
     });
 });
 
+
+eventoContainer.addEventListener('click', () => {
+    modalVerDetalleEvento.classList.remove('hidden-force');
+    modalVerDetalleEvento.showModal();
+
+});
+
+
+btnEliminarEvento.addEventListener('click', () => {
+    eventoContainer.remove();
+    modalVerDetalleEvento.close();
+});
