f789114d76e9463b16e82950241d9ba0239d6795
cris <54333719+andreedev@users.noreply.github.com>
2026-01-23 20:35:49 +0100

    cambios en el header y el home

--- js/header.js
+++ js/header.js
@@ -41,6 +41,7 @@ class HomeHeader extends HTMLElement {
                 </div>
                 <div class="header-buttons-desktop-container">
                     <a href="./noticias.html" class="header-button">Noticias</a>
+                    <a href="./eventos.html" class="header-button">Eventos</a>
                     <a href="./preguntas-frecuentes.html" class="header-button">Preguntas frecuentes</a>
                     <a href="./gala.html" class="header-button">Gala</a>
                     <a href="./bases-legales.html" class="header-button">Bases legales</a>
