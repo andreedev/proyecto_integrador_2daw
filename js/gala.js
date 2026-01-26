73c5e82657c0652ee30cc11b791af91d5ee64d43
maria <mariaqingxuan@gmail.com>
2026-01-26 19:45:56 +0100

    pre-evento hecho

--- js/gala.js
+++ js/gala.js
@@ -1,32 +1,32 @@
-const track = document.querySelector('.carousel-track');
-const items = document.querySelectorAll('.carousel-item');
-const prevBtn = document.querySelector('.prev');
-const nextBtn = document.querySelector('.next');
-const dots = document.querySelectorAll('.dot');
+// const track = document.querySelector('.carousel-track');
+// const items = document.querySelectorAll('.carousel-item');
+// const prevBtn = document.querySelector('.prev');
+// const nextBtn = document.querySelector('.next');
+// const dots = document.querySelectorAll('.dot');
 
-let index = 0;
+// let index = 0;
 
-function updateCarousel() {
-    track.style.transform = `translateX(-${index * 100}%)`;
+// function updateCarousel() {
+//     track.style.transform = `translateX(-${index * 100}%)`;
 
-    dots.forEach(dot => dot.classList.remove('active'));
-    dots[index].classList.add('active');
-}
+//     dots.forEach(dot => dot.classList.remove('active'));
+//     dots[index].classList.add('active');
+// }
 
-nextBtn.addEventListener('click', () => {
-    index = (index + 1) % items.length;
-    updateCarousel();
-});
+// nextBtn.addEventListener('click', () => {
+//     index = (index + 1) % items.length;
+//     updateCarousel();
+// });
 
-prevBtn.addEventListener('click', () => {
-    index = (index - 1 + items.length) % items.length;
-    updateCarousel();
-});
+// prevBtn.addEventListener('click', () => {
+//     index = (index - 1 + items.length) % items.length;
+//     updateCarousel();
+// });
 
-dots.forEach(dot => {
-    dot.addEventListener('click', () => {
-        index = parseInt(dot.dataset.index);
-        updateCarousel();
-    });
-});
+// dots.forEach(dot => {
+//     dot.addEventListener('click', () => {
+//         index = parseInt(dot.dataset.index);
+//         updateCarousel();
+//     });
+// });
 
