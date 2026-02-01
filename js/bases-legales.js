c5694a43a0156366feec50e46b3d498e7decfca8
maria <mariaqingxuan@gmail.com>
2026-02-01 04:01:54 +0100

    cambios en bases legales

--- js/bases-legales.js
+++
@@ -1,8 +0,0 @@
-const contenido = document.querySelector('#contenido');
-
-async function cargarContenido() {
-    const response = await obtenerBasesLegales();
-    contenido.innerHTML = response.data;
-}
-
-cargarContenido();
