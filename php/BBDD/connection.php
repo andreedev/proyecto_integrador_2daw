8c8a32f71097ef54482cb5c9aae9da56b607aee0
cris <54333719+andreedev@users.noreply.github.com>
2026-02-03 09:49:42 +0100

    ajusta la configuración de conexión a la base de datos para pruebas locales

--- php/BBDD/connection.php
+++ php/BBDD/connection.php
@@ -4,21 +4,17 @@
 // * descomentar para probar localmente
 // * comentar esto para probar en una BD compartida
 // */
-//$servidor = "localhost";
-//$usuario = "root";
-//$password = "";
-//$db = "festival_cortos";
-//$port = 3306;
+$servidor = "localhost";
+$usuario = "root";
+$password = "";
+$db = "festival_cortos";
+$port = 3306;
 
 /**
  * comentar esto para probar localmente
  * descomentar esto para probar en una BD compartida
  */
- $servidor = "proyecto-integrador-db-aereodb.c.aivencloud.com";
- $usuario = "avnadmin";
- $password = "AVNS_Lh2lXbxWC0nSTMynXuH";
- $db = "festival_cortos";
- $port = 20236;
+// $servidor 2
 
 function abrirConexion() {
     global $conexion, $servidor, $usuario, $password, $port, $db;
