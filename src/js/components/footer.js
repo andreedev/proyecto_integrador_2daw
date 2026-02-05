dbbb7bbf1de88956da2acf1fef8a1204011f65b7
cris <54333719+andreedev@users.noreply.github.com>
2026-02-06 00:15:11 +0100

    mejora el diseño y la responsividad del footer

--- src/js/components/footer.js
+++ src/js/components/footer.js
@@ -19,27 +19,27 @@ class HomeFooter extends HTMLElement {
     render() {
         this.innerHTML = `
             <div class="w-100 bg-neutral-01 position-relative">
-                <img src="../img/Vector-ue.svg" alt="Logo" class="footer-image ue-logo position-absolute bottom-68px right-24px">
+                <img src="../img/Vector-ue.svg" alt="logo universidad europea" class="super-placed-logo ue-logo position-absolute">
                 
-                <div class="d-flex flex-column flex-md-row justify-space-between align-items-center p-24px w-100 gap-32px m-auto flex-wrap container-md-to-lg pb-80px pt-24px">
-                    <div class="footer-container-1">
-                        <img src="../img/canon-logo.png" alt="Logo" class="footer-image">
+                <div class="footer-container d-flex flex-column flex-md-row justify-content-center justify-space-between align-items-center w-100 gap-16px m-auto flex-wrap container-lg-to-xl">
+                    <div class="sponsor-container d-flex justify-content-center align-items-center gap-16px order-2 order-md-1 gap-32px">
+                        <img src="../img/canon-logo.png" alt="logo canon" class="w-160px">
                     </div>
-                    <div class="footer-container-2">
-                        <p class="footer-text">C/Tajo, s/n, 28670</p>
-                        <p class="footer-text">dpo@universidadeuropea.es</p>
+                    <div class="d-flex flex-column justify-content-center align-items-center gap-16px order-3 order-md-2">
+                        <span class="fw-500 text-neutral-09">C/Tajo, s/n, 28670</span>
+                        <span class="fw-500 text-neutral-09">dpo@universidadeuropea.es</span>
                     </div>
-                    <div class="footer-container-3">
-                        <a class="footer-secondary-text" href="#">Política de privacidad</a>
-                        <a class="footer-secondary-text" href="./bases-legales.html">Bases legales</a>
+                    <div class="d-flex flex-column justify-content-center align-items-center gap-16px order-4 order-md-3">
+                        <a class="fw-500 text-neutral-09" href="#">Política de privacidad</a>
+                        <a class="fw-500 text-neutral-09" href="./bases-legales.html">Bases legales</a>
                     </div>
-                    <div class="footer-container-4">
+                    <div class="d-flex flex-column gap-16px order-1 order-md-4">
                         <div class="social-media-container">
-                            <span class="w-24px h-24px bg-neutral-09 icon-facebook"></span>
-                            <span class="w-24px h-24px bg-neutral-09 icon-instagram"></span>
-                            <span class="w-24px h-24px bg-neutral-09 icon-youtube"></span>
-                            <span class="w-24px h-24px bg-neutral-09 icon-linkedin"></span>
-                            <span class="w-24px h-24px bg-neutral-09 icon-twitter"></span>
+                            <a href="https://www.facebook.com/UnivEuropea" target="_blank" class="w-24px h-24px bg-neutral-09 hover-bg-primary-01 transition-all cursor-pointer icon-facebook"></a>
+                            <a href="https://www.instagram.com/ueuropea" target="_blank" class="w-24px h-24px bg-neutral-09 hover-bg-primary-01 transition-all cursor-pointer icon-instagram"></a>
+                            <a href="http://youtube.com/univeuropea" target="_blank" class="w-27px h-24px bg-neutral-09 hover-bg-primary-01 transition-all cursor-pointer icon-youtube"></a>
+                            <a href="https://www.linkedin.com/school/universidad-europea-de-madrid" target="_blank" class="w-24px h-24px bg-neutral-09 hover-bg-primary-01 transition-all cursor-pointer icon-linkedin"></a>
+                            <a href="https://x.com/ueuropea" target="_blank" class="w-24px h-24px bg-neutral-09 hover-bg-primary-01 transition-all cursor-pointer icon-twitter"></a>
                         </div>
                     </div>
                 </div>
