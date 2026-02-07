98944caa0f8d236de2e86308ba6b557067ba082a
maria <mariaqingxuan@gmail.com>
2026-02-07 14:46:44 +0100

    cambios en preguntas frecuentes

--- src/js/preguntas-frecuentes.js
+++ src/js/preguntas-frecuentes.js
@@ -1,19 +1,19 @@
 const faqData = [
     {
         question: "¿Quiénes pueden participar exactamente?",
-        answer: "Pueden participar estudiantes actuales de Ciclos Formativos, Grado o Máster de la UE.También antiguos alumnos(Alumni) que se hayan graduado en 2024/2025 (Categoría UE) o en los últimos cinco cursos académicos desde el 2019/2020 (Categoría Alumni)."
+        answer: "Pueden participar alumnos actuales de Ciclos Formativos, Grado o Máster de la UE. También antiguos alumnos(Alumni) que se hayan graduado en 2024/2025 (Categoría UE) o en los últimos cinco cursos académicos desde el 2019/2020 (Categoría Alumni)."
     },
     {
         question: "¿El concurso es individual o por equipos?",
-        answer: "La inscipción es individual.Como participante, deberás aportar tu número de expediente y documento de identidad oficial"
+        answer: "La inscipción es individual. Como participante, deberás aportar tu número de expediente y documento de identidad oficial."
     },
     {
         question: "Si soy Alumni de hace 3 años, ¿en qué categoría entro?",
-        answer: "Entrarías en la categoría 'Mejor Cortometraje Alumni', destinada a graduados de los cursos 2019/20 hasta el 2023/24"
+        answer: "Entrarías en la categoría 'Mejor Cortometraje Alumni', destinada a graduados de los cursos 2019/20 hasta el 2023/24."
     },
     {
         question: "¿Qué duración debe tener mi corto?",
-        answer: "La duración máxima  es de 20 minutos, incluyendo los títulos de crédito. ¡Asegúrate de no pasarte ni un segundo!."
+        answer: "La duración máxima  es de 20 minutos, incluyendo los títulos de crédito. ¡Asegúrate de no pasarte ni un segundo!"
     },
     {
         question: "¿Puedo presentarlo en un idioma que no sea castellano?",
@@ -21,11 +21,14 @@ const faqData = [
     },
     {
         question: "¿Cuáles son las especificaciones técnicas del archivo?",
-        answer: "Para que tu corto se vea perfecto en la gala, debe cumpplir estos requisitos:Formato: QuickTime MOV (H.264) o MP4. Resolución: 1920X1080p a 25 fps. Aspecto: 16:9 con audio estéreo."
+        answer: "Para que tu corto se vea perfecto en la gala, debe cumpplir estos requisitos:"+"<br>"+
+            "Formato: QuickTime MOV (H.264) o MP4. " + "<br>"+
+            "Resolución: 1920X1080p a 25 fps. " + "<br>"+
+            "Aspecto: 16:9 con audio estéreo."
     },
     {
         question: "¿Cómo se eligen a los ganadores?",
-        answer: "El concurso tiene dos frases. primero, un comité de profesores realizará una Fase de Visionado para elegir 3 finalistas por categoría. Después, un jurado experto decidirá los ganadores definitivos."
+        answer: "El concurso tiene dos frases: primero, un comité de profesores realizará una Fase de Visionado para elegir 3 finalistas por categoría. Después, un jurado experto decidirá los ganadores definitivos."
     }
 ];
 
